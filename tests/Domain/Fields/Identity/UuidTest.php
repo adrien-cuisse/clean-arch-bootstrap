1c54a76124b607e53a4d49a81b9bd70fac2b674f
adrien-cuisse <a.cuisse@epitech.eu>
2021-07-17 20:07:10 +0200

    private properties based tests removed

--- tests/Domain/Fields/Identity/UuidTest.php
+++ tests/Domain/Fields/Identity/UuidTest.php
@@ -20,19 +20,19 @@ use Alphonse\CleanArch\Domain\Fields\Identity\Uuid\InvalidUuidTimeHighBytesCount
  */
 final class UuidTest extends TestCase
 {
-    private function getProperty(UuidInterface $uuid, string $propertyName): int|array
-    {
-        $class = new ReflectionClass(Uuid::class);
-        $property = $class->getProperty(name: $propertyName);
-        $property->setAccessible(true);
+    // private function getProperty(UuidInterface $uuid, string $propertyName): int|array
+    // {
+    //     $class = new ReflectionClass(Uuid::class);
+    //     $property = $class->getProperty(name: $propertyName);
+    //     $property->setAccessible(true);
 
-        return $property->getValue(object: $uuid);
-    }
+    //     return $property->getValue(object: $uuid);
+    // }
 
-    private function getConstant(string $constantName): int
-    {
-        return (new ReflectionClass(Uuid::class))->getConstant(name: $constantName);
-    }
+    // private function getConstant(string $constantName): int
+    // {
+    //     return (new ReflectionClass(Uuid::class))->getConstant(name: $constantName);
+    // }
 
     /**
      * @return UuidInterface - an instance to test
@@ -136,76 +136,76 @@ final class UuidTest extends TestCase
         // then instantation should be rejected
     }
 
-    public function overflowingBytesProvider(): Generator
-    {
-        yield 'time-low' => [
-            ['timeLowBytes' => [0x4201, 0x4202, 0x4203, 0x4204]],
-            [0x01,0x02, 0x03, 0x04],
-            'timeLowBytes'
-        ];
-        yield 'time-mid' => [
-            ['timeMidBytes' => [0x4205, 0x4206]],
-            [0x05,0x06],
-            'timeMidBytes'
-        ];
-        yield 'time-high' => [
-            ['timeHighBytes' => [0x4207, 0x4208]],
-            [0x07,0x08],
-            'timeHighBytes'
-        ];
-        yield 'node' => [
-            ['nodeBytes' => [0x420a, 0x420b, 0x420c, 0x420d, 0x420e, 0x420f]],
-            [0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f],
-            'nodeBytes'
-        ];
-    }
+    // public function overflowingBytesProvider(): Generator
+    // {
+    //     yield 'time-low' => [
+    //         ['timeLowBytes' => [0x4201, 0x4202, 0x4203, 0x4204]],
+    //         [0x01,0x02, 0x03, 0x04],
+    //         'timeLowBytes'
+    //     ];
+    //     yield 'time-mid' => [
+    //         ['timeMidBytes' => [0x4205, 0x4206]],
+    //         [0x05,0x06],
+    //         'timeMidBytes'
+    //     ];
+    //     yield 'time-high' => [
+    //         ['timeHighBytes' => [0x4207, 0x4208]],
+    //         [0x07,0x08],
+    //         'timeHighBytes'
+    //     ];
+    //     yield 'node' => [
+    //         ['nodeBytes' => [0x420a, 0x420b, 0x420c, 0x420d, 0x420e, 0x420f]],
+    //         [0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f],
+    //         'nodeBytes'
+    //     ];
+    // }
 
-    /**
+    /*
      * @test
      * @dataProvider overflowingBytesProvider
      * @covers ::__construct
      * @covers ::clampToByte
      * @covers ::clampToBytes
      */
-    public function clamps_to_bytes(array $overflowingBytes, array $expectedClampedValues, string $propertyName): void
-    {
-        // given a new Uuid
-        $uuid = $this->createInstance(...$overflowingBytes);
+    // public function clamps_to_bytes(array $overflowingBytes, array $expectedClampedValues, string $propertyName): void
+    // {
+    //     // given a new Uuid
+    //     $uuid = $this->createInstance(...$overflowingBytes);
 
-        // when accessing the stored bytes
-        $storedBytes = $this->getProperty(uuid: $uuid, propertyName: $propertyName);
+    //     // when accessing the stored bytes
+    //     $storedBytes = $this->getProperty(uuid: $uuid, propertyName: $propertyName);
 
-        // then they should be clamped as expected
-        $this->assertSame(
-            expected: $expectedClampedValues,
-            actual: $storedBytes,
-            message: sprintf(
-                "Expected property {$propertyName} to be clamped to [%s], got [%s]",
-                implode(', ', $expectedClampedValues),
-                implode(', ', $storedBytes),
-            ),
-        );
-    }
+    //     // then they should be clamped as expected
+    //     $this->assertSame(
+    //         expected: $expectedClampedValues,
+    //         actual: $storedBytes,
+    //         message: sprintf(
+    //             "Expected property {$propertyName} to be clamped to [%s], got [%s]",
+    //             implode(', ', $expectedClampedValues),
+    //             implode(', ', $storedBytes),
+    //         ),
+    //     );
+    // }
 
-    /**
+    /*
      * @test
      * @covers ::__construct
      */
-    public function clamps_time_high_most_significant_byte_to_4_bits(): void
-    {
-        // given some uuid with time-high first byte taking more than 4 bits
-        $uuid = $this->createInstance(timeHighBytes: [0b1111_1111, 0x00]);
+    // public function clamps_time_high_most_significant_byte_to_4_bits(): void
+    // {
+    //     // given some uuid with time-high first byte taking more than 4 bits
+    //     $uuid = $this->createInstance(timeHighBytes: [0b1111_1111, 0x00]);
 
-        // when accessing its time-high first byte
-        $timeHighFirstByte = $this->getProperty($uuid, 'timeHighBytes')[0];
+    //     // when accessing its time-high first byte
+    //     $timeHighFirstByte = $this->getProperty($uuid, 'timeHighBytes')[0];
 
-        // then the byte should be clamped to 4 bits
-        $this->assertSame(
-            expected: 0b0000_1111,
-            actual: $timeHighFirstByte,
-            message: "Expected time-high MSB to get clamped to 4 bits"
-        );
-    }
+    //     // then the byte should be clamped to 4 bits
+    //     $this->assertSame(
+    //         expected: 0b0000_1111,
+    //         actual: $timeHighFirstByte,
+    //         message: "Expected time-high MSB to get clamped to 4 bits"
+    //     );
+    // }
 
     /**
      * @test
@@ -230,25 +230,25 @@ final class UuidTest extends TestCase
         );
     }
 
-    /**
+    /*
      * @test
      * @covers ::__construct
      */
-    public function clamps_clock_seq_high_to_6_bits(): void
-    {
-        // given some uuid with clock-seq-high byte taking more than 6 bits
-        $uuid = $this->createInstance(clockSeqHighByte: 0b1111_1111);
+    // public function clamps_clock_seq_high_to_6_bits(): void
+    // {
+    //     // given some uuid with clock-seq-high byte taking more than 6 bits
+    //     $uuid = $this->createInstance(clockSeqHighByte: 0b1111_1111);
 
-        // when accessing its clock-seq-high byte
-        $clockSeqHighByte = $this->getProperty($uuid, 'clockSeqHighBits');
+    //     // when accessing its clock-seq-high byte
+    //     $clockSeqHighByte = $this->getProperty($uuid, 'clockSeqHighBits');
 
-        // then the byte should be clamped to 6 bits
-        $this->assertSame(
-            expected: 0b0011_1111,
-            actual: $clockSeqHighByte,
-            message: "Expected clock-seq-high to get clamped to 6 bits"
-        );
-    }
+    //     // then the byte should be clamped to 6 bits
+    //     $this->assertSame(
+    //         expected: 0b0011_1111,
+    //         actual: $clockSeqHighByte,
+    //         message: "Expected clock-seq-high to get clamped to 6 bits"
+    //     );
+    // }
 
     /**
      * @test
@@ -277,6 +277,8 @@ final class UuidTest extends TestCase
      * @test
      * @covers ::__construct
      * @covers ::__toString
+     * @covers ::clampToByte
+     * @covers ::clampToBytes
      * @covers ::toRfcUuidString
      * @covers ::hexaStringFrom
      */
@@ -296,7 +298,7 @@ final class UuidTest extends TestCase
         );
     }
 
-    /**
+    /*
      * @test
      * @covers ::__construct
      * @covers ::__toString
@@ -304,48 +306,48 @@ final class UuidTest extends TestCase
      * @covers ::getVersion
      * @covers ::hexaStringFrom
      */
-    public function contains_version_in_byte_6_most_significant_bits(): void
-    {
-        // given some Uuid
-        $uuid = $this->createInstance(version: 0b0000_0101);
+    // public function contains_version_in_byte_6_most_significant_bits(): void
+    // {
+    //     // given some Uuid
+    //     $uuid = $this->createInstance(version: 0b0000_0101);
 
-        // when extracting its version digit and its version
-        $versionDigit = $uuid->toRfcUuidString()[14];
-        $version = $uuid->getVersion();
+    //     // when extracting its version digit and its version
+    //     $versionDigit = $uuid->toRfcUuidString()[14];
+    //     $version = $uuid->getVersion();
 
-        // then they should be the same
-        $this->assertEquals(
-            actual: $versionDigit,
-            expected: $version,
-            message: "Uuid should have its version number as 13th digit, found {$versionDigit} instead of {$version}",
-        );
-    }
+    //     // then they should be the same
+    //     $this->assertEquals(
+    //         actual: $versionDigit,
+    //         expected: $version,
+    //         message: "Uuid should have its version number as 13th digit, found {$versionDigit} instead of {$version}",
+    //     );
+    // }
 
-    /**
+    /*
      * @test
      * @covers ::__toString
      * @covers ::toRfcUuidString
      * @covers ::hexaStringFrom
      */
-    public function contains_variant_in_byte_8_most_significant_bits(): void
-    {
-        // given a new Uuid
-        $uuid = $this->createInstance();
+    // public function contains_variant_in_byte_8_most_significant_bits(): void
+    // {
+    //     // given a new Uuid
+    //     $uuid = $this->createInstance();
 
-        // when extracting its 9th byte of data (byte 8, digits 19 and 20)
-        $rfcRepresentation = (string) $uuid;
-        $ninthByteHexaString = substr($rfcRepresentation, 19, 2);
-        sscanf($ninthByteHexaString, '%2x', $ninthByte);
-        // and when accessing its variant bits
-        $variantBits = $this->getConstant('VARIANT_BITS');
+    //     // when extracting its 9th byte of data (byte 8, digits 19 and 20)
+    //     $rfcRepresentation = (string) $uuid;
+    //     $ninthByteHexaString = substr($rfcRepresentation, 19, 2);
+    //     sscanf($ninthByteHexaString, '%2x', $ninthByte);
+    //     // and when accessing its variant bits
+    //     $variantBits = $this->getConstant('VARIANT_BITS');
 
-        // then it should have its two most significant bytes set to its variant
-        $this->assertSame(
-            expected: $variantBits,
-            actual: $ninthByte & 0b1100_0000,
-            message: "Uuid should have the most significant bits of its byte 8 set to its variant",
-        );
-    }
+    //     // then it should have its two most significant bytes set to its variant
+    //     $this->assertSame(
+    //         expected: $variantBits,
+    //         actual: $ninthByte & 0b1100_0000,
+    //         message: "Uuid should have the most significant bits of its byte 8 set to its variant",
+    //     );
+    // }
 
     public function byteProvider(): Generator
     {
@@ -504,73 +506,73 @@ final class UuidTest extends TestCase
         // then instantiation should be rejected
     }
 
-    public function uuidStringToBytesProvider(): Generator
-    {
-        yield 'time-low' => [
-            '12345678-0000-0000-0000-000000000000',
-            'timeLowBytes',
-            [0x12, 0x34, 0x56, 0x78],
-        ];
-        yield 'time-mid' => [
-            '00000000-9abc-0000-0000-000000000000',
-            'timeMidBytes',
-            [0x9a, 0xbc],
-        ];
-        yield 'version' => [
-            '00000000-0000-b000-0000-000000000000',
-            'version',
-            0xb,
-        ];
-        yield 'version bits' => [
-            '00000000-0000-4000-0000-000000000000',
-            'versionBits',
-            0b0100_0000,
-        ];
-        yield 'time-high' => [
-            '00000000-0000-0ef0-0000-000000000000',
-            'timeHighBytes',
-            [0x0e, 0xf0],
-        ];
-        yield 'clock-seq-high' => [
-            '00000000-0000-0000-2d00-000000000000',
-            'clockSeqHighBits',
-            0x2d,
-        ];
-        yield 'clock-seq-low' => [
-            '00000000-0000-0000-00ef-000000000000',
-            'clockSeqLowByte',
-            0xef,
-        ];
-        yield 'node' => [
-            '00000000-0000-0000-0000-456789abcdef',
-            'nodeBytes',
-            [0x45, 0x67, 0x89, 0xab, 0xcd, 0xef],
-        ];
-    }
+    // public function uuidStringToBytesProvider(): Generator
+    // {
+    //     yield 'time-low' => [
+    //         '12345678-0000-0000-0000-000000000000',
+    //         'timeLowBytes',
+    //         [0x12, 0x34, 0x56, 0x78],
+    //     ];
+    //     yield 'time-mid' => [
+    //         '00000000-9abc-0000-0000-000000000000',
+    //         'timeMidBytes',
+    //         [0x9a, 0xbc],
+    //     ];
+    //     yield 'version' => [
+    //         '00000000-0000-b000-0000-000000000000',
+    //         'version',
+    //         0xb,
+    //     ];
+    //     yield 'version bits' => [
+    //         '00000000-0000-4000-0000-000000000000',
+    //         'versionBits',
+    //         0b0100_0000,
+    //     ];
+    //     yield 'time-high' => [
+    //         '00000000-0000-0ef0-0000-000000000000',
+    //         'timeHighBytes',
+    //         [0x0e, 0xf0],
+    //     ];
+    //     yield 'clock-seq-high' => [
+    //         '00000000-0000-0000-2d00-000000000000',
+    //         'clockSeqHighBits',
+    //         0x2d,
+    //     ];
+    //     yield 'clock-seq-low' => [
+    //         '00000000-0000-0000-00ef-000000000000',
+    //         'clockSeqLowByte',
+    //         0xef,
+    //     ];
+    //     yield 'node' => [
+    //         '00000000-0000-0000-0000-456789abcdef',
+    //         'nodeBytes',
+    //         [0x45, 0x67, 0x89, 0xab, 0xcd, 0xef],
+    //     ];
+    // }
 
-    /**
+    /*
      * @test
      * @dataProvider uuidStringToBytesProvider
      * @covers ::fromString
      */
-    public function parses_uuid_string_and_store_bytes_correctly(string $uuidString, string $propertyName, int|array $expectedValue): void
-    {
-        // given an Uuid made from a string
-        $uuid = call_user_func_array(
-            callback: $this->createInstance()::class . '::fromString',
-            args: [$uuidString],
-        );
+    // public function parses_uuid_string_and_store_bytes_correctly(string $uuidString, string $propertyName, int|array $expectedValue): void
+    // {
+    //     // given an Uuid made from a string
+    //     $uuid = call_user_func_array(
+    //         callback: $this->createInstance()::class . '::fromString',
+    //         args: [$uuidString],
+    //     );
 
-        // when accessing its stored bytes
-        $bytes = $this->getProperty($uuid, $propertyName);
+    //     // when accessing its stored bytes
+    //     $bytes = $this->getProperty($uuid, $propertyName);
 
-        // then it should match the expectation
-        $this->assertSame(
-            expected: $expectedValue,
-            actual: $bytes,
-            message: "Expected {$propertyName} to be correctly assigned",
-        );
-    }
+    //     // then it should match the expectation
+    //     $this->assertSame(
+    //         expected: $expectedValue,
+    //         actual: $bytes,
+    //         message: "Expected {$propertyName} to be correctly assigned",
+    //     );
+    // }
 
     /**
      * @test
@@ -594,6 +596,30 @@ final class UuidTest extends TestCase
             expected: $uuidString,
             actual: (string) $uuid,
             message: "Expected to find back uuid-string {$uuidString} when make an Uuid from it, got {$uuid}",
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::fromString
+     * @covers ::getVersion
+     */
+    public function creates_versioned_uuid_from_string(): void
+    {
+        // given an versioned uuid-string
+        $uuidString = '00000000-0000-7000-0000-000000000000';
+
+        // when making an uuid from it
+        $uuid = $uuid = call_user_func_array(
+            callback: $this->createInstance()::class . '::fromString',
+            args: [$uuidString],
+        );
+
+        // then version of the instance should be 7
+        $this->assertSame(
+            expected: 7,
+            actual: $uuid->getVersion(),
+            message: "Expected version 7 to be parsed from string, got {$uuid->getVersion()}",
         );
     }
 }
