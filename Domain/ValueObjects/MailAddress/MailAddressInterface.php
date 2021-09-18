231ea45b1e9f8b4096a0bda508c9151a9dc05837
Adrien Cuisse <adrien.cuisse@epitech.eu>
2021-09-18 14:44:41 +0200

    useless comment removed

--- Domain/ValueObjects/MailAddress/MailAddressInterface.php
+++ Domain/ValueObjects/MailAddress/MailAddressInterface.php
@@ -5,9 +5,6 @@ namespace Alphonse\CleanArchBootstrap\Domain\ValueObjects\MailAddress;
 use Alphonse\CleanArchBootstrap\Domain\ValueObjects\ValueObjectInterface;
 use Stringable;
 
-/**
- * A mail address
- */
 interface MailAddressInterface extends Stringable, ValueObjectInterface
 {
 }
