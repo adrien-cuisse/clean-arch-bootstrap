6766548c415ea61723daa12c0442794297f7c693
Adrien Cuisse <adrien.cuisse@epitech.eu>
2021-09-18 14:45:05 +0200

    useless comment removed

--- Domain/ValueObjects/PhoneNumber/PhoneNumberInterface.php
+++ Domain/ValueObjects/PhoneNumber/PhoneNumberInterface.php
@@ -5,9 +5,6 @@ namespace Alphonse\CleanArchBootstrap\Domain\ValueObjects\PhoneNumber;
 use Stringable;
 use Alphonse\CleanArchBootstrap\Domain\ValueObjects\ValueObjectInterface;
 
-/**
- * A phone number
- */
 interface PhoneNumberInterface extends Stringable, ValueObjectInterface
 {
     public function toNationalFormat(): string;
